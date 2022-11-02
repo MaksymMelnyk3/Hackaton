8404c9ac507e398d2f2bec626d78b39316af497f
Maksym <ya.maksimka228@gmail.com>
2022-11-02 22:24:24 +0200

    initial commit

---
+++ Hackaton/Hackaton/HackatonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackatonApp.swift
+//  Hackaton
+//
+//  Created by Maxim Melnik on 02.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct HackatonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
